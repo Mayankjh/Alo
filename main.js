1c16370849a9d4b8ec02894b34f8c261f85987d9
Mayank Jha <mayankjha7722@gmail.com>
2018-08-05 13:39:20 +0530

    indicators added!!

--- main.js
+++ main.js
@@ -3,14 +3,21 @@ $(function() {
     var $panorama = $('.panorama');
 
     var left = $panorama.offset().left;
+    var right = $panorama.offset().right;
     var width = $panorama.width();
 
-    $('.panorama').mousemove(function(e) {
+    $('.left').click(function(e) {
         var offset = e.pageX - left;
         var percentage = offset / width * 100;
 
         $panorama.css('background-position', percentage + '% 0');
 
     });
+    $('.right').click(function(e) {
+        var offset = e.pageX - left;
+        var percentage = offset / width * 100;
 
+        $panorama.css('background-position', percentage + '% 0');
+
+    });
 });
